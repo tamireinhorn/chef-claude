085b9ed55d396bc7274f0186ca2bce62104dded1
Tamir <tamtamireinhorn@gmail.com>
2026-08-22 17:58:22 -0300

    header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+
+    return (
+        <header>
+            <div className="header-div">
+                <img src="src/assets/chef-claude-icon.png" alt="" />
+                <h1>Chef Claude</h1>
+           </div>
+
+        </header>
+    )
+}
