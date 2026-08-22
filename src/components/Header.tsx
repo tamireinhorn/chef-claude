584a80b763ea18f5a8b2630476b9d9cbd2779ab5
Tamir <tamtamireinhorn@gmail.com>
2026-08-22 18:01:05 -0300

    no need for a div

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,10 @@ export default function Header() {
 
     return (
         <header>
-            <div className="header-div">
-                <img src="src/assets/chef-claude-icon.png" alt="" />
-                <h1>Chef Claude</h1>
-           </div>
+            
+            <img src="src/assets/chef-claude-icon.png" alt="" />
+            <h1>Chef Claude</h1>
+
 
         </header>
     )
