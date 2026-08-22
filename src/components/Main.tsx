fef2a6c99a011a8f2d4136a97e7b3cebb1aa992b
Tamir <tamtamireinhorn@gmail.com>
2026-08-22 18:18:59 -0300

    form

---
+++ src/components/Main.tsx
@@ -0,0 +1,14 @@
+export default function Main() {
+    return (
+        <main>
+            <form className="add-ingredient-form">
+                <input 
+                aria-label="Add ingredient"
+                type="text"
+                placeholder="e.g oregano"
+                />
+                <button>Add ingredient</button>
+            </form>
+        </main>
+    )
+} 
